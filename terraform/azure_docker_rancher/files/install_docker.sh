875f886aae937c26132c5ff3449f1f521ef1d968
Arvind Iyengar <arvind.iyengar@rancher.com>
2023-09-27 14:18:18 -0700

    Add sudo apt update step to make docker install more reliable and prevent hanging apply on Rancher module

--- terraform/azure_docker_rancher/files/install_docker.sh
+++ terraform/azure_docker_rancher/files/install_docker.sh
@@ -1,3 +1,15 @@
+if [ -r /etc/os-release ]; then
+    lsb_dist="$(. /etc/os-release && echo "$ID")"
+fi
+
+case "$lsb_dist" in
+	ubuntu|debian|raspbian)
+        echo "Running sudo apt update..."
+        sudo apt update -qq
+    ;;
+esac
+
+echo "Installing Docker..."
 curl https://releases.rancher.com/install-docker/${docker_version}.sh | sh;
 
 return=1;
